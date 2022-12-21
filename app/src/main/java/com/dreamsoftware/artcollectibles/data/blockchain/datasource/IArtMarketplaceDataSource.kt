7e3a80e70121092b790b0f0adaa8e60c2fed312b
Sergio Sánchez <sss4esob@gmail.com>
2022-12-21 11:03:48 +0100

    [Web3j] Implementing Web3J datasource layer

--- app/src/main/java/com/dreamsoftware/artcollectibles/data/blockchain/datasource/IArtMarketplaceDataSource.kt
+++ app/src/main/java/com/dreamsoftware/artcollectibles/data/blockchain/datasource/IArtMarketplaceDataSource.kt
@@ -2,6 +2,7 @@ package com.dreamsoftware.artcollectibles.data.blockchain.datasource
 
 import com.dreamsoftware.artcollectibles.data.blockchain.entity.ArtCollectibleForSaleEntity
 import kotlinx.coroutines.flow.Flow
+import java.math.BigInteger
 
 interface IArtMarketplaceDataSource {
 
@@ -19,5 +20,30 @@ interface IArtMarketplaceDataSource {
      * Fetch market items that are owned by the current authenticated user
      */
     suspend fun fetchOwnedMarketItems(): Flow<ArtCollectibleForSaleEntity>
+
+    /**
+     * Allow us to fetch market history
+     */
+    suspend fun fetchMarketHistory(): Flow<ArtCollectibleForSaleEntity>
+
+    /**
+     * list an item with a `tokenId` for a `price`
+     * @param tokenId
+     * @param price
+     */
+    suspend fun putItemForSale(tokenId: BigInteger, price: BigInteger): Flow<BigInteger>
+
+    /**
+     * Cancel a listing of an item with a `tokenId`
+     * @param tokenId
+     */
+    suspend fun withdrawFromSale(tokenId: BigInteger)
+
+    /**
+     * Buy an item with a `tokenId` and pay the owner and the creator
+     * @param tokenId
+     * @param price
+     */
+    suspend fun buyItem(tokenId: BigInteger, price: BigInteger)
 
 }
--- app/src/main/java/com/dreamsoftware/artcollectibles/data/blockchain/di/BlockchainModule.kt
+++ app/src/main/java/com/dreamsoftware/artcollectibles/data/blockchain/di/BlockchainModule.kt
@@ -47,7 +47,6 @@ class BlockchainModule {
     ))
 
 
-
     /**
      * Provide Art Collectible Mapper
      */
